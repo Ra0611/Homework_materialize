16d2113b1cee7943f00f2572a1fbfe6c4ffabcb6
Ra0611 <an3611nt@gmail.com>
2021-04-11 21:19:03 +0300

    cleaned js

--- js/my_screepts.js
+++ js/my_screepts.js
@@ -1,20 +1,10 @@
-document.addEventListener('DOMContentLoaded', function () {
-    var elems = document.querySelectorAll('.sidenav');
-    var instances = M.Sidenav.init(elems, options);
+
+$(document).ready(function () {
+    $('.button-collapse').sideNav();
 });
 $(document).ready(function () {
-    $(".button-collapse").sideNav();
+    $('.parallax').parallax();
 });
-$(document).ready(function () {
-    $(".parallax").parallax();
-});
-
-document.addEventListener('DOMContentLoaded', function () {
-    var elems = document.querySelectorAll('.carousel');
-    var instances = M.Carousel.init(elems, options);
-});
-
-// Or with jQuery
 
 $(document).ready(function () {
     $('.carousel').carousel();
